ea9325176934f138d616ddeb506a7c50425c47c9
mhhundvin <mhhundvin@hotmail.com>
2021-10-06 13:11:31 +0200

    Started on experiment 2

---
+++ experiment1/src/test/java/no/hvl/dat250/expass6/experiment1/Experiment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.expass6.experiment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
