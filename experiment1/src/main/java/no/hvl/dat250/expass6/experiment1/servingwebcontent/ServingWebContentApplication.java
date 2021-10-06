987d58621cbf32bc65dff2eb89ac7a295bb83375
mhhundvin <mhhundvin@hotmail.com>
2021-10-06 12:55:01 +0200

    Experiment 1

---
+++ experiment1/src/main/java/no/hvl/dat250/expass6/experiment1/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.expass6.experiment1.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
